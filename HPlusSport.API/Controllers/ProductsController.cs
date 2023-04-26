78152c7df00f5e2d1f3d01e17fb85786fe52fe62
RogerXavier <rogerxavier10@gmail.com>
2023-04-25 22:45:27 -0300

    Controllers and actions

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -10,30 +10,10 @@ namespace HPlusSport.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly ShopContext _context;
-
-        public ProductsController (ShopContext context)
-        {
-            _context = context;
-
-            _context.Database.EnsureCreated();
-        }
-         
         [HttpGet]
-        public async Task<ActionResult> GetAllProducts()
+        public void GetProducts()
         {
-            return Ok(await _context.Products.ToListAsync());
-        }
 
-        [HttpGet] [Route("{id}")]
-        public async Task<ActionResult> GetProduct(int id)
-        {
-            var product = await _context.Products.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return Ok(product);
         }
     }
 }
